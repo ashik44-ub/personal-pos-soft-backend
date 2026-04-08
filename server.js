69668c5fec0373bd074363bf6e36ca02e30011ab
ashik44-ub <shopnilashik4@gmail.com>
2026-04-08 20:56:31 +0600

    update link

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://personal-store-lime.vercel.app', 'https://personal-store-lime.vercel.app'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes Middleware
